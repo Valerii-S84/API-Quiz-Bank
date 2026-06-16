d0996c800de1d4f993e384fea7418f827f404316
Quiz Arena <local@quiz-arena>
2026-06-16 18:05:17 +0200

    perf(selection): drive queue claim validation from candidates

--- src/quizbank_mvp/selection_queue_fast_path_sql.py
+++ src/quizbank_mvp/selection_queue_fast_path_sql.py
@@ -5,7 +5,9 @@ from __future__ import annotations
 
 POSTGRESQL_CLAIM_ITEM_SQL_TEMPLATE = """
         WITH queue_candidates AS MATERIALIZED (
-            SELECT sqi.queue_item_id, sqi.queue_id, sqi.item_id, sqi.position
+            SELECT sqi.queue_item_id, sqi.queue_id, sqi.item_id, sqi.position,
+                   sq.language_code, sq.content_bank_id, sq.bank_version_id,
+                   sq.cefr_level, sq.theme_id, sq.objective_id, sq.pattern_id
             FROM selection_queue_items sqi
             JOIN selection_queues sq ON sq.queue_id = sqi.queue_id
             WHERE sqi.queue_id IN ({queue_placeholders})
@@ -15,23 +17,26 @@ POSTGRESQL_CLAIM_ITEM_SQL_TEMPLATE = """
             ORDER BY sqi.position ASC, sqi.queue_item_id ASC
             LIMIT ?
         ),
+        valid_candidates AS MATERIALIZED (
+            SELECT qc.queue_item_id, qc.position
+            FROM queue_candidates qc
+            JOIN quiz_items qi ON qi.item_id = qc.item_id
+            WHERE qi.status IN ({status_placeholders})
+              AND qi.language_code = qc.language_code
+              AND qi.content_bank_id = qc.content_bank_id
+              AND qi.bank_version_id = qc.bank_version_id
+              AND (qc.cefr_level = '' OR qi.sublevel = qc.cefr_level)
+              AND (qc.theme_id = '' OR qi.theme_id = qc.theme_id)
+              AND (qc.objective_id = '' OR qi.objective_id = qc.objective_id)
+              AND (qc.pattern_id = '' OR qi.pattern_id = qc.pattern_id)
+            ORDER BY qc.position ASC, qc.queue_item_id ASC
+        ),
         candidate AS (
             SELECT sqi.queue_item_id
-            FROM queue_candidates qc
-            JOIN selection_queue_items sqi ON sqi.queue_item_id = qc.queue_item_id
-            JOIN selection_queues sq ON sq.queue_id = qc.queue_id
-            JOIN quiz_items qi ON qi.item_id = sqi.item_id
-            WHERE sq.queue_status IN ('ready', 'draining')
-              AND sqi.claim_status = 'available'
-              AND qi.status IN ({status_placeholders})
-              AND qi.language_code = sq.language_code
-              AND qi.content_bank_id = sq.content_bank_id
-              AND qi.bank_version_id = sq.bank_version_id
-              AND (sq.cefr_level = '' OR qi.sublevel = sq.cefr_level)
-              AND (sq.theme_id = '' OR qi.theme_id = sq.theme_id)
-              AND (sq.objective_id = '' OR qi.objective_id = sq.objective_id)
-              AND (sq.pattern_id = '' OR qi.pattern_id = sq.pattern_id)
-            ORDER BY qc.position ASC, qc.queue_item_id ASC
+            FROM valid_candidates vc
+            JOIN selection_queue_items sqi ON sqi.queue_item_id = vc.queue_item_id
+            WHERE sqi.claim_status = 'available'
+            ORDER BY vc.position ASC, vc.queue_item_id ASC
             LIMIT 1
             FOR UPDATE OF sqi SKIP LOCKED
         ),
